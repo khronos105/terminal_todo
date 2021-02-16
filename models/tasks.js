db93ae59452577d08acabddd7679334d3551af68
Roma Bilibov <romabilibov@gmail.com>
2021-02-16 12:31:21 +0100

    Created Task and Tasks models

---
+++ models/tasks.js
@@ -0,0 +1,13 @@
+/**
+ * _list:
+ *  { 'uuid-1234515-123131-2: { id:12, desc: asd, completedAt: 1231231} }
+ */
+class Tasks{
+    _list = {};
+
+    constructor(){
+        this._list = {};
+    }
+}
+
+module.exports = Tasks;
